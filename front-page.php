262aeec8dbc40f6e06b682dcd4b607505f97e836
Roman <roman985475612@gmail.com>
2021-03-22 10:21:23 +0300

    Chapter 9 complete

--- front-page.php
+++ front-page.php
@@ -53,7 +53,15 @@ if( $query->have_posts() ): ?>
         </div>
     </div>
 <?php endif ?>
+
 <?php $page_id = get_page_by_path( 'home' )->ID ?>
+
+<?php
+$query = new WP_Query([
+    'post_type'         => 'progress',
+    'posts_per_page'    => 4,
+]);
+if( $query->have_posts() ): ?>
 <div id="fh5co-content">
     <div class="video fh5co-video" style="background-image: url(<?= get_template_directory_uri() ?>/assets/images/video.jpg);">
         <a href="<?= get_post_meta( $page_id, 'video', true) ?>" class="popup-vimeo"><i class="icon-video2"></i></a>
@@ -64,7 +72,102 @@ if( $query->have_posts() ): ?>
             <h2>Why Choose Us?</h2>
             <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts far from the countries Vokalia and Consonantia, there live the blind texts. </p>
         </div>
+        <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:<?= get_post_meta($post->ID, 'progress', true) ?>%">
+                    <?= the_title() ?> <?= get_post_meta($post->ID, 'progress', true) ?>%
+                </div>
+            </div>
+        <?php endwhile ?>
+        <?php wp_reset_postdata() ?>
     </div>
 </div>
+<?php endif ?>
+
+
+<div id="fh5co-practice" class="fh5co-bg-section">
+    <div class="container">
+        <div class="row animate-box">
+            <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+                <h2>Our Legal Practice Area</h2>
+                <p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 text-center animate-box">
+                <div class="services">
+                    <span class="icon">
+                        <i class="icon-home"></i>
+                    </span>
+                    <div class="desc">
+                        <h3><a href="#">Real Estate Law</a></h3>
+                        <p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 text-center animate-box">
+                <div class="services">
+                    <span class="icon">
+                        <i class="icon-eye"></i>
+                    </span>
+                    <div class="desc">
+                        <h3><a href="#">Insurance Law</a></h3>
+                        <p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 text-center animate-box">
+                <div class="services">
+                    <span class="icon">
+                        <i class="icon-shopping-cart"></i>
+                    </span>
+                    <div class="desc">
+                        <h3><a href="#">Business Law</a></h3>
+                        <p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 text-center animate-box">
+                <div class="services">
+                    <span class="icon">
+                        <i class="icon-umbrella"></i>
+                    </span>
+                    <div class="desc">
+                        <h3><a href="#">Personal Injury</a></h3>
+                        <p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 text-center animate-box">
+                <div class="services">
+                    <span class="icon">
+                        <i class="icon-heart"></i>
+                    </span>
+                    <div class="desc">
+                        <h3><a href="#">Medical Neglegence</a></h3>
+                        <p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 text-center animate-box">
+                <div class="services">
+                    <span class="icon">
+                        <i class="icon-help"></i>
+                    </span>
+                    <div class="desc">
+                        <h3><a href="#">Criminal Defense</a></h3>
+                        <p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12 text-center animate-box">
+                <p><a class="btn btn-primary btn-lg btn-learn" href="#">View More</a></p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
 
 <?php get_footer(); ?>
--- inc/custom-posttype.php
+++ inc/custom-posttype.php
@@ -19,3 +19,22 @@ function law_counter_posttype()
     );
 }
 add_action( 'init', 'law_counter_posttype', 0 );
+
+function law_progress_posttype()
+{
+    register_post_type( 'Progress', [
+            'labels'        => [
+                'name'          => __( 'Progress', 'law' ),
+                'singular_name' => __( 'Progress', 'law' ),
+            ],
+            'public'        => true,
+            'has_archive'   => true,
+            'rewrite'       => true,
+            'supports'      => [
+                'title',
+                'custom-fields',
+            ],
+        ]
+    );
+}
+add_action( 'init', 'law_progress_posttype', 0 );
